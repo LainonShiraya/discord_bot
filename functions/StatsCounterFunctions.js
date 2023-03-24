dec9a77b87296c63d7f2e71b2d0072f415e014fe
KonradRzodkiewicz <konradmmo@gmail.com>
2023-03-24 21:45:22 +0100

    removed unused functions

--- functions/StatsCounterFunctions.js
+++ functions/StatsCounterFunctions.js
@@ -1,37 +1,22 @@
-const deckGamesPlayed = decklist => {
-  return decklist.wins + decklist.loses;
-};
+const deckWinPerc = (decklist) => {
+  if (decklist.Games === 0) {
+    return 0
+  } else {
+    const number = decklist.Wins / decklist.Games
+    return (number * 100).toFixed(2)
+  }
+}
 
-const deckWinPerc = decklist => {
-	if(decklist.Games === 0){
-		return 0;
-	}
-	else {
-  const number = decklist.Wins / decklist.Games;
-  return (number * 100).toFixed(2);
-	}
-};
-
-const playerWinPerc = (wins,games) => {
-	if(games === 0){
-		return 0;
-	} else {
-  number = wins / games;
-  return (number * 100).toFixed(2);
-	}
-};
-
-const playerGamesPlayed = user => {
-  let gamesPlayed = 0;
-  user.decklists.map(
-    decklist => (gamesPlayed += decklist.wins + decklist.loses)
-  );
-  return gamesPlayed;
-};
+const playerWinPerc = (wins, games) => {
+  if (games === 0) {
+    return 0
+  } else {
+    number = wins / games
+    return (number * 100).toFixed(2)
+  }
+}
 
 module.exports = {
-  playerGamesPlayed,
   playerWinPerc,
-  deckGamesPlayed,
   deckWinPerc,
-};
+}
